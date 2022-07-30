98b703f06f5359e9497bde2ee7f1b39b6ed27eee
Python-Ali <110286912+Python-Ali@users.noreply.github.com>
2022-07-30 20:46:00 +0200

    Update Time⌚ tables.py

--- Time⌚ tables.py
+++ Time⌚ tables.py
@@ -1,3 +1,4 @@
 table = int(input("Please enter a times table: "))
 for x in range(0,101):
     print (x, "x", table, "=", x*table)
+# This is a python program so it probably will not work for other languages
