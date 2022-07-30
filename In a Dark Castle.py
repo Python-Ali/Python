2d38c25dff46ed2fc1fc78b3f906d3951c118a2a
Python-Ali <110286912+Python-Ali@users.noreply.github.com>
2022-07-30 20:31:50 +0200

    Add files via upload

---
+++ In a Dark Castle.py
@@ -0,0 +1,7 @@
+print("You are in a dark castle, you have to answer the qeustion right to survive")
+choice = input("5 * 3 / 5 * 9 : ")
+if choice == "27":    print("You chose door one, and got a big black bag of gold  You won!")
+else:    print("You chose door 2 and got eaten up by a dragon  Game over!")
+name = input("What is you name?: ")
+if choice == "27":    print("Well done", name, "You won!😎")
+else:    print("Good try", name, "but you lost😕")
