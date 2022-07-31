a5bf1cf24bb89ced17c43f8a04f0f38ab862abe9
Python-Ali <110286912+Python-Ali@users.noreply.github.com>
2022-07-31 18:16:07 +0200

    Add files via upload

---
+++ code language.py
@@ -0,0 +1,10 @@
+alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZ"
+StrinEncry = input("Please enter a message to encrypt: ")
+StrinEncry = StrinEncry.upper()
+shiftAmount = int(input("Please enter a whole number from 1-25 to be your key."))
+encrystring = ""
+for currentCharacter in StrinEncry:
+    position = alpha.find(currentCharacter)
+    newPosition = position + shiftAmount
+    encrystring = encrystring + alpha[newPosition]
+print("Your encrypted message is", encrystring)
